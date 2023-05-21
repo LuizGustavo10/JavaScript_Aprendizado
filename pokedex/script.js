8b21b0cb8a311426a4e035ef4c76cb5192b12952
LuizGustavo10 <gustavo180g@gmail.com>
2023-05-20 22:12:03 -0300

    pokedex mais outros projetos javascript

--- pokedex/script.js
+++ pokedex/script.js
@@ -7,11 +7,12 @@ function search() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            
             // Manipule os dados recebidos para exibir as informações do Pokémon
             var pokemonInfo = document.getElementById("pokemonInfo");
             pokemonInfo.innerHTML = `
                 <h2>${data.name.toUpperCase()}</h2>
-                <img src="${data.sprites.front_default}" alt="${data.name}">
+                <img class="imgPoke" src="${data.sprites.front_default}" alt="${data.name}">
                 <p>ID: ${data.id}</p>
                 <p>Tipo: ${data.types.map(type => type.type.name).join(", ")}</p>
                 <p>Habilidades: ${data.abilities.map(ability => ability.ability.name).join(", ")}</p>
